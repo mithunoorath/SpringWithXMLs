f10736304d2e0ebc40c364f7a7071dce1d1b6a36
vagrant <vagrant@localhost>
2017-11-30 22:38:17 -0500

    java doc included

--- src/main/java/mithun/learn/samples/implementations/EmployeeDetails.java
+++ src/main/java/mithun/learn/samples/implementations/EmployeeDetails.java
@@ -1,9 +1,18 @@
 package mithun.learn.samples.implementations;
 
-import mithun.learn.samples.interfaces.Printer;
+import mithun.learn.samples.interfaces.PrinterIntfc;
 
-public class EmployeeDetails implements Printer {
+/**EmployeeDetails class implements the PrinterIntfc.
+ * @author Mithun Oorath
+ * @version 1.0.0
+ *
+ */
 
+public class EmployeeDetails implements PrinterIntfc {
+
+	/**
+	 * @return String
+	 */
 	public String printName() {
 		// TODO Auto-generated method stub
 		String message ="Hi My name is Mithun Oorath";
--- src/main/java/mithun/learn/samples/serviceBean/EmployeeService.java
+++ src/main/java/mithun/learn/samples/serviceBean/EmployeeService.java
@@ -1,18 +1,58 @@
 package mithun.learn.samples.serviceBean;
 
 import mithun.learn.samples.implementations.EmployeeDetails;
-import mithun.learn.samples.interfaces.Printer;
+import mithun.learn.samples.interfaces.PrinterIntfc;
 
+/** One of the main Spring service class.
+ * @author Mithun Oorath
+ *
+ */
 public class EmployeeService {
-	private Printer details;
+	private PrinterIntfc details;
+	private EmployeeDetails employeeDetails;
 	
-	public EmployeeService(Printer details) {
+	/**
+	 * 
+	 * @return PrinterInftc implemented object.
+	 */
+	public PrinterIntfc getDetails() {
+		return details;
+	}
+
+
+	/**This is to show construction injection.
+	 * 
+	 * @param PrinterIntfc implemented object.
+	 */
+
+	public EmployeeService(PrinterIntfc details) {
 		this.details = details;
 		System.out.println("construction injection successful");
 	}
 	
+	/**This is to show the setter injection
+	 * 
+	 * @param EmployeeDetails
+	 */
+			
+	public void setEmployeeDetails(EmployeeDetails employeeDetails) {
+		this.employeeDetails = employeeDetails;
+		
+		System.out.println("Setter injection successful using Employee details class");
+	}
+
+
+	/**
+	 * Protected constructor for Spring.
+	 */
+	protected EmployeeService() {}
+	
+	
+	/**
+	 * Call of the printInfo method.
+	 */
 	public void printInfo() {
-		details.printName();
+		employeeDetails.printName();
 	}
 
 }
